f8df4d3b3293c2605ce63534784ac3a4d36b407e
Ahmad Eknan <00330365@rijnijssel.nl>
2024-12-16 08:56:15 +0100

    Title veranderd

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(isset($_GET['page'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dit is de index</title>
     <link rel="stylesheet" href="css/edit.css">
 </head>
     <body>
